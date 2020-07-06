fdcfa35668b0ebaec5d3638c5d177a9ba42eac42
Flavio Santos <flavio@flavio.lan>
2020-07-06 09:24:40 -0300

    database include

---
+++ js/db-shtn.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyD5vAood5D7FSz0UcpszH0Lfdi42JXaHOE",
+    authDomain: "datastore-gxps4.firebaseapp.com",
+    databaseURL: "https://datastore-gxps4.firebaseio.com",
+    projectId: "datastore-gxps4",
+    storageBucket: "datastore-gxps4.appspot.com",
+    messagingSenderId: "849238427431",
+    appId: "1:849238427431:web:9eeeaf00cd0b4bdfcc8b02",
+    measurementId: "G-F8BRV2QMGR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
