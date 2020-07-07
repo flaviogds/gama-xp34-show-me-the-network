40c781b7e06a6ae9eec1b4f368b07c130fb09d96
Flavio Santos <flavio@flavio.lan>
2020-07-07 12:18:06 -0300

    Integracao das paginas adicionais

--- js/main.js
+++ js/main.js
@@ -8,13 +8,21 @@ function addData() {
 
     var data = { nome: nome, email: email};
 
-    firebase.firestore().collection('leads').add(data).then(docRef =>{
-        console.log(docRef.id);
-    })
+    if(nome!="" && email!=""){
+      firebase.firestore().collection('leads').add(data).then(docRef =>{
+        console.log(docRef.id);window.location.href='thank-you.html'; 
+     })
+    }
+    if (nome=="") {
+      document.getElementById("fail-nome").innerHTML = "Por favor nos diga seu nome."      
+    }
+    if (email=="") {
+      document.getElementById("fail-email").innerHTML = "Por favor nos diga seu email." 
+    }
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 9, 2020 15:37:25").getTime();
+var countDownDate = new Date("Jul 9, 2020 19:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -26,16 +34,24 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var hours = Math.floor((distance / (1000 * 60 * 60)));
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="countdown_timer"
-    document.getElementById("timer").innerHTML = hours + " : " + minutes + " : " + seconds;
-
+  if(days > 1){
+    document.getElementById("timer").innerHTML = days + " dias " + hours + "h " + minutes + "min " + seconds+"s";
+  }
+  if(days == 1){
+    document.getElementById("timer").innerHTML = days + " dia " + hours + "h " + minutes + "min " + seconds+"s";
+  }
+  if (days < 1) {
+    document.getElementById("timer").innerHTML = hours + "h " + minutes + "min " + seconds+"s";
+  }
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown_timer").innerHTML = "AGORA!";
+    document.getElementById("timer").innerHTML = "AGORA!";
   }
 }, 1000);
